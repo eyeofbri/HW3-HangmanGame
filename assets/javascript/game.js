b2cdae57ce1a50d7c94c0ec0cceeb48680e17386
brian mclendon <brianmclendon@brians-MacBook-Pro.local>
2017-02-06 15:14:19 -0500

    Structured the js file more and created some functions that save code from being spaghetti.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ var gameOver = false;
 var freeLetters = 0;
 var canShowHowTo = true;
 var BG_position = 0;
+var currentSlimeNumber = 0;
 
 var pHealth = 0; //player health "hearts"
 var pScore = 0; //player score
@@ -64,11 +65,11 @@ var words_foods = [
 "green beans", "Guancamole", "gumbov", "grits", 
 "Graham crackers", "ham", "halibut", "hamburger", 
 "honey", "huenos rancheros", "hash browns", "hot dogs", 
-"hummus", "ice cream", "Irish stew", 
-"Indian food", "Italian bread", "jambalaya", "jelly", 
+"hummus", "ice cream", "stew", 
+"tikka masala", "Garlic bread", "jambalaya", "jelly", 
 "jerky", "jalapeño", "kale", "kabobs", "ketchup", "kiwi", 
 "kidney beans", "kingfish", "lobster", "Lamb", "Linguinev", 
-"Lasagna", "Meatballs", "Moose", "Milk", "Milkshake", "Noodles", 
+"Lasagna", "Meatballs", "Milk", "Milkshake", "Noodles", 
 "Ostrich", "Pizza", "Pepperoni", "Porter", "Pancakes", 
 "Quesadilla", "Quiche", "Reuben", "Spinach", "Spaghetti", 
 "Tater tots", "Toast", "Venison", "Waffles", "Wine", "Walnuts", 
@@ -96,12 +97,13 @@ $(document).ready (function(){
 			if(freeLetters != keyClicked ){
 				if(!pressedKeys.includes(keyClicked)){
 					pressedKeys.push(keyClicked);
-					keyAI(keyClicked);
+					keyAI("keyHit", keyClicked);
 				}
 			}else{
 				gameFunctions("useFree");
 			}
 		}
+
 		if(!canStartGame && gameStarted && gameOver ){
 			gameFunctions("reset");
 		}
@@ -118,7 +120,7 @@ $(document).ready (function(){
 			if(isKeyALetter(e.which)){
 				if(!pressedKeys.includes(keyClicked)){
 					pressedKeys.push(keyClicked);
-					keyAI(keyClicked);
+					keyAI("keyHit", keyClicked);
 				}
 			}
 
@@ -141,78 +143,88 @@ $(document).ready (function(){
 		}
 	}
 
-	function keyAI(k) {
+	function keyAI(whatToDo, k) {
+		if(whatToDo == "keyHit"){
+			canAttack = false; //STOP further "attacks"... during this function
 
-		canAttack = false; //STOP further "attacks"... during this function
+			//LOOP through the letter buttons and do important things
+			var letter_buttons = document.getElementsByTagName("button");
+			for (var lb = 0; lb < letter_buttons.length; lb++) {
 
-		//LOOP through the letter buttons and do important things
-		var letter_buttons = document.getElementsByTagName("button");
-		for (var lb = 0; lb < letter_buttons.length; lb++) {
+	    		var currentButton = document.getElementsByTagName("button")[lb];
 
-    		var currentButton = document.getElementsByTagName("button")[lb];
+				//FIND the button "lb" corresponding to your pressed key "k"
+				if(k == currentButton.innerHTML){
 
-			//FIND the button "lb" corresponding to your pressed key "k"
-			if(k == currentButton.innerHTML){
+	    			//CHECK if pressed key is in the current hidden word				
+	    			k = k.toUpperCase();
+	    			var word = currentWord.toUpperCase();
+	    			if (word.includes(k)) {
 
-    			//CHECK if pressed key is in the current hidden word				
-    			k = k.toUpperCase();
-    			var word = currentWord.toUpperCase();
-    			if (word.includes(k)) {
+	    				//YOU GUESSED A LETTER CORRECTLY!
 
-    				//YOU GUESSED AN ENTIRE WORD CORRECTLY!
+	    				adjustStats("pScore", "raise");
 
-    				adjustStats("pScore", "raise");
+	    				currentButton.setAttribute("id", "key-picked_PASS");
 
-    				currentButton.setAttribute("id", "key-picked_PASS");
+	    				//UPDATE the hidden text to display 
+	    				//by looping through the letters of the current word
+	    				for (var c=0; c < word.length; c++) { 
 
-    				//UPDATE the hidden text to display 
-    				//by looping through the letters of the current word
-    				for (var c=0; c < word.length; c++) { 
+	    					//if the letter mattches k
+	    					if(word.charAt(c) == k){
+	 							
+	 							//split the hidden text letters into an array
+								var hiddenText = $( "#spellText" ).text().split(" ");
+								hiddenText.pop(); //remove some garbage
+								hiddenText[c] = k; //update correctly guessed letter
 
-    					//if the letter mattches k
-    					if(word.charAt(c) == k){
- 							
- 							//split the hidden text letters into an array
-							var hiddenText = $( "#spellText" ).text().split(" ");
-							hiddenText.pop(); //remove some garbage
-							hiddenText[c] = k; //update correctly guessed letter
+	    						$( "#spellText" ).empty(); //CLEAR hidden word display before updating
 
-    						$( "#spellText" ).empty(); //CLEAR hidden word display before updating
+	    						//replace the corresponding _ (hidden letter) with guessed letter
+	    						for (var t = 0; t < hiddenText.length; t++) {
+	    							$( "#spellText" ).append(hiddenText[t]+" ");
+	    						}
 
-    						//replace the corresponding _ (hidden letter) with guessed letter
-    						for (var t = 0; t < hiddenText.length; t++) {
-    							$( "#spellText" ).append(hiddenText[t]+" ");
-    						}
+	    						// IF hidden text CONTAINS no more  _(hidden letters)
+	    						if(!hiddenText.includes("_")){
+	    							console.log("Player Completed a word!");
 
-    						// IF hidden text CONTAINS no more  _(hidden letters)
-    						if(!hiddenText.includes("_")){
-    							console.log("Player Completed a word!");
+	    							animationController("playerAttack");
 
-    							animationController("playerAttack");
-
-    							adjustStats("pScore", "raise");
-    							adjustStats("pHealth", "raise");
-    							enemyAI("hit");
-    						}
-    					} 
-    				}
-    			}else{
-    				//IF the prssed key ISNT within the current hidden word...
-    				animationController("enemyAttack");
-
-    				//player loses health AND or Game Over
-	    			if(pHealth >0){
-	    				setTimeout(function(){
-	    					adjustStats("pHealth", "lower");
-	    				 }, 700);
-	    				currentButton.setAttribute("id", "key-picked_FAIL");
+	    							adjustStats("pScore", "raise");
+	    							adjustStats("pHealth", "raise");
+	    							enemyAI("hit");
+	    						}
+	    					} 
+	    				}
 	    			}else{
-	    				gameFunctions("gameOver");
+	    				//IF the pressed key ISNT within the current hidden word...
+	    				animationController("enemyAttack");
+
+	    				//player loses health AND or Game Over
+		    			if(pHealth >0){
+		    				fastTimeout("lower_pHealth",700);
+		    				currentButton.setAttribute("id", "key-picked_FAIL");
+		    			}else{
+		    				gameFunctions("gameOver");
+		    			}
+	    				
 	    			}
-    				
-    			}
-    		}
+	    		}
+	    	}
     	}
+
+    	if(whatToDo === "resetKeys"){
+			
+			pressedKeys = [];
+			pressedKeys.length = 0;
+			
+			var allKeys = document.getElementsByTagName("button");
+			for (var i = 0; i < allKeys.length; i++) {
+				allKeys[i].setAttribute("id", "");
+			}
+		}
     }
 
 	//////////////
@@ -256,51 +268,25 @@ $(document).ready (function(){
 
 			gameOver = false;
 
-			$( "#startButton_H" ).children(":first").removeClass('restart').addClass('start');
+			startButtonAI("reset");
 
 			$( "#hero" ).removeClass('spin');
     	}
 
     	if(which === "start"){
 
-    		//FADE the start Button
-    		$( "#startButton_H" ).children(":first").attr("class", "startB_fade");
-    		
-    		//FADE the start button holder
-    		$( "#startButton_H" ).attr("class", "startB_H_fade");
-    		//HIDE the Start Button Holder after a time
-    		setTimeout(function(){
-    			$( "#startButton_H" ).attr("class", "startB_H_hide");
-    		}, 500);
+    		startButtonAI("hide");
 
     		canStartGame = false;
-    		
 
     		adjustStats("infoText", "");
     		adjustStats("spellText", "");
     		adjustStats("alertText", "");
     		adjustStats("pHealth", "start");
 
-    		gameFunctions("howTo");
+    		howTo_popup();
 
     		gameFunctions("newLevel");
-    	}
-
-    	if(which === "howTo"){
-    		if(canShowHowTo){
-    			canShowHowTo = false;
-	    		$( "#howTo" ).removeClass('howTo_hide').addClass('howTo_show');
-
-				setTimeout(function(){		
-		    		$( "#howTo" ).click(function() {
-		  				$( "#howTo" ).removeClass('howTo_show').addClass('howTo_hide');
-		  				
-		  				setTimeout(function(){ gameStarted = true; }, 700);
-					});
-	    		}, 700);
-			}else{
-				gameStarted = true;
-			}
     	}
 
     	if(which === "newLevel"){
@@ -308,45 +294,38 @@ $(document).ready (function(){
     		enemyAI("newEnemy");
     		gameFunctions("newWord");
 
-    		BG_position -=100;
-    		document.getElementById("gameBG").style.backgroundPosition = BG_position+"%";
+			animationController("bg");
     	}
 
     	if(which === "newWord"){
 
     		var lastWord = currentWord;
 
-    		gameFunctions("resetKeys");
+    		keyAI("resetKeys");
+
     		$( "#spellText" ).empty();
 
-    		var rand_Word_TYPE = Math.floor((Math.random() * 2) + 1);
+    		var rand_Word_TYPE = returnRandom(2,1);
 
     		if(rand_Word_TYPE == 1){
-    			currentWord = words_countries[Math.floor((Math.random() * words_countries.length))];
-    			if(lastWord == currentWord){
-    				currentWord = words_countries[Math.floor((Math.random() * words_countries.length))];
+    			currentWord = words_countries[returnRandom(words_countries.length,0)];
+    			if(lastWord === currentWord){
+    				currentWord = words_countries[returnRandom(words_countries.length,0)];
     			}
+    			lastWord = currentWord;
+
     			animateLetters(currentWord, "Country! &#x1F30E");
     		}else{
-    			currentWord = words_foods[Math.floor((Math.random() * words_foods.length))];
-    			if(lastWord == currentWord){
-	    			currentWord = words_foods[Math.floor((Math.random() * words_foods.length))];
+    			currentWord = words_foods[returnRandom(words_foods.length,0)];
+    			if(lastWord === currentWord){
+	    			currentWord = words_foods[returnRandom(words_foods.length,0)];
     			}
-    			animateLetters(currentWord, "Food! &#x1F354");
+    			lastWord = currentWord;
+
+    			animateLetters(currentWord, "Food &#x1F354 and Drinks &#127866!");
     		}
 
 		    console.log("Current Word: "+currentWord); //CHEATERS UNCOMMENT THIS
-		}
-
-		if(which === "resetKeys"){
-			
-			pressedKeys = [];
-			pressedKeys.length = 0;
-			
-			var allKeys = document.getElementsByTagName("button");
-			for (var i = 0; i < allKeys.length; i++) {
-				allKeys[i].setAttribute("id", "");
-			}
 		}
 
 		if(which === "gameOver"){
@@ -357,20 +336,10 @@ $(document).ready (function(){
 			adjustStats("spellText", "GAME OVER");
 			adjustStats("alertText", "Your last word was: " + currentWord);
 
-			//bring the start button back up!
-			//FADE the start Button IN
-    		$( "#startButton_H" ).children(":first").attr("class", "");
-    		//FADE the start button holder IN
-    		$( "#startButton_H" ).attr("class", "");
-    		$( "#startButton_H" ).children(":first").removeClass('start').addClass('restart');
+			startButtonAI("showRestart");
 
-    		setTimeout(function(){
-    			gameOver = true;
-    		}, 2000);
-
-    		setTimeout(function(){
-    			animationController("playerDeath");	
-    		}, 700);
+    		fastTimeout("gameOver",2000);
+    		fastTimeout("anim_pDeath",700);
 		}
 
 		if(which === "useFree" && freeLetters >0){
@@ -392,11 +361,12 @@ $(document).ready (function(){
 	    			
 					//SIMULATE KEYPRESS
 					pressedKeys.push(currentWord.split("")[t].toUpperCase());
-					keyAI(currentWord.split("")[t].toUpperCase());
+					keyAI("keyHit", currentWord.split("")[t].toUpperCase());
 	    			break;
 	    		}
     		}
 		}
+
 	}
 
 	function animateLetters(word, type){
@@ -426,48 +396,47 @@ $(document).ready (function(){
 		//PLAYER HEALTH
 		if (which == "pHealth") {
 
-			if(how == "reset"){ pHealth = 0;}
-			if(how == "start"){ pHealth = 10;}
-			if(how == "lower"){	pHealth -= 1; 
-								alertPOP("Lost 1 Health!");
-							    shakeThings("hero");
-								tintThings("hero");
-								shakeThings("health_UI_H");
-
-
-								$( "#health" ).removeClass('upArrow').removeClass('downArrow').addClass('downArrow');
-								//need this to MAKE SURE ITS Removed when done
-								setTimeout(function(){ $( "#health" ).removeClass('downArrow') }, 700);
-								
-								}
-			if(how == "raise"){ pHealth += 1; 
-								alertPOP("Gained 1 Health!");
-								shakeThings("health_UI_H");
-
-								$( "#health" ).removeClass('upArrow').removeClass('downArrow').addClass('upArrow');
-								//need this to MAKE SURE ITS Removed when done
-								setTimeout(function(){ $( "#health" ).removeClass('upArrow') }, 700);
-								
-								}
+			if(how == "reset"){ 
+				pHealth = 0;
+			}
+			if(how == "start"){ 
+				pHealth = 10;
+			}
+			if(how == "lower"){	
+				pHealth -= 1; 
+				alertPOP("Lost 1 Health!");
+				shakeThings("hero");
+				tintThings("hero");
+				shakeThings("health_UI_H");
+				arrowUI("#health","downArrow");
+			}
+			if(how == "raise"){ 
+				pHealth += 1; 
+				alertPOP("Gained 1 Health!");
+				shakeThings("health_UI_H");
+				arrowUI("#health","upArrow");
+			}
 
 			$( "#health" ).html(pHealth);
 		}
 
 		//PLAYER SCORE
 		if (which == "pScore") {
-			if(how == "reset"){	pScore = 0;}
-			if(how == "raise"){	pScore += 1; 
-								alertPOP("+1 Score!");
-								shakeThings("score_UI_H");
-								
-								$( "#score" ).removeClass('upArrow').addClass('upArrow');
-								//need this to MAKE SURE ITS Removed when done
-								setTimeout(function(){ $( "#score" ).removeClass('upArrow') }, 700);
-								
-								}
-			if(how == "word"){ 	pScore += 2; 
-								alertPOP("Guessed the word! Score +2");
-								shakeThings("score_UI_H");}
+
+			if(how == "reset"){	
+				pScore = 0;
+			}
+			if(how == "raise"){	
+				pScore += 1; 
+				alertPOP("+1 Score!");
+				shakeThings("score_UI_H");
+				arrowUI("#score","upArrow");
+			}
+			if(how == "word"){
+				pScore += 2; 
+				alertPOP("Guessed the word! Score +2");
+				shakeThings("score_UI_H");
+			}
 
 			$( "#score" ).html(pScore);
 		}
@@ -499,38 +468,123 @@ $(document).ready (function(){
 
     function tintThings(el) {
     	var thingToTint = document.getElementById(el);
-
+    	var oldFilter = thingToTint.style.filter;
 		thingToTint.style.filter = "grayscale(100%) sepia(100%) hue-rotate(300deg)";
 
-		setTimeout(function(){ thingToTint.style.filter = "grayscale(0%) sepia(0%) hue-rotate()"; }, 500);
+		setTimeout(function(){ thingToTint.style.filter = oldFilter; }, 500);
     }
 
     function animationController(which) {
 
-		if(which == "playerAttack"){
+		if(which === "playerAttack"){
 			//PLAYER ATTACK ANIM
 			$( "#wizardAttack" ).removeClass('W_attack').addClass('W_attack');
 			//need this to MAKE SURE ITS Removed when done
 		    setTimeout(function(){ $( "#wizardAttack" ).removeClass('W_attack'); }, 1000);
 		}
 
-		if(which == "playerDeath"){
+		if(which === "playerDeath"){
 			//PLAYER ATTACK ANIM
 			$( "#hero" ).removeClass('spin').addClass('spin');
 		}
 
-		if(which == "enemyAttack"){
+		if(which === "enemyAttack"){
 	    	//ENEMY ATTACK ANIM
 			$( "#enemyAttack" ).removeClass('E_attack').addClass('E_attack');
 			//need this to MAKE SURE ITS Removed when done
 			setTimeout(function(){ $( "#enemyAttack" ).removeClass('E_attack'); }, 1000);
 		}
 
-		if(which == "enemyDeath"){
+		if(which === "enemyDeath"){
 			//PLAYER ATTACK ANIM
 			$( "#enemy" ).removeClass('spin').addClass('spin');
 		}
 
+		if(which === "bg"){
+			BG_position -=100;
+    		document.getElementById("gameBG").style.backgroundPosition = BG_position+"%";
+		}
+
+    }
+
+    function startButtonAI(whatToDo) {
+    	if(whatToDo === "reset"){
+    		$( "#startButton_H" ).children(":first").removeClass('restart').addClass('start');
+    	}
+
+    	if(whatToDo === "hide"){
+    		//FADE the start Button
+    		$( "#startButton_H" ).children(":first").attr("class", "startB_fade");
+    		
+    		//FADE the start button holder
+    		$( "#startButton_H" ).attr("class", "startB_H_fade");
+    		//HIDE the Start Button Holder after a time
+    		setTimeout(function(){
+    			$( "#startButton_H" ).attr("class", "startB_H_hide");
+    		}, 500);
+    	}
+
+    	if(whatToDo ==="showRestart"){
+    		//bring the start button back up!
+			//FADE the start Button IN
+    		$( "#startButton_H" ).children(":first").attr("class", "");
+    		//FADE the start button holder IN
+    		$( "#startButton_H" ).attr("class", "");
+    		$( "#startButton_H" ).children(":first").removeClass('start').addClass('restart');
+    	}
+    }
+
+
+    function howTo_popup() {
+   		if(canShowHowTo){
+    		canShowHowTo = false;
+	   		$( "#howTo" ).removeClass('howTo_hide').addClass('howTo_show');
+
+			setTimeout(function(){		
+		   		$( "#howTo" ).click(function() {
+		  			$( "#howTo" ).removeClass('howTo_show').addClass('howTo_hide');
+		  			
+		  			setTimeout(function(){ gameStarted = true; }, 700);
+		  		});
+
+				$( document ).keypress(function() {
+					$( "#howTo" ).removeClass('howTo_show').addClass('howTo_hide');
+		  			$("#howTo").removeAttr("keypress");
+		  			setTimeout(function(){ gameStarted = true; }, 700);
+				});
+	    	}, 700);
+		}else{
+			gameStarted = true;
+		}
+    }
+
+    function returnRandom(max, min) {
+    	return Math.floor((Math.random() * max) + min);
+    }
+
+    function fastTimeout(which, time) {
+    	if(which === "lower_pHealth"){
+	    	setTimeout(function(){ adjustStats("pHealth", "lower"); }, time);	
+    	}
+
+    	if(which === "gameOver"){
+    		setTimeout(function(){ gameOver = true; }, time);
+    	}
+
+    	if(which === "anim_pDeath"){
+    		setTimeout(function(){ animationController("playerDeath"); }, time);
+    	}
+    }
+
+    function arrowUI(where, whichArrow) {
+    	//first try to remove both types, to be safe and clear garbage
+		$( where ).removeClass('upArrow').removeClass('downArrow');
+
+		//now add the new one, with a quick redundancy to make sure its not already added!
+		$( where ).removeClass(whichArrow).addClass(whichArrow);
+
+		//need this to MAKE SURE ITS Removed when done
+		setTimeout(function(){ $( where ).removeClass(whichArrow) }, 700);			
     }
 
 	//////////////
@@ -551,6 +605,9 @@ $(document).ready (function(){
 
 			//pick enemy # and grab image
     		var rand_Slime = Math.floor((Math.random() * 26) + 1);
+    		//check for duplicate enemy types, and if so, redo!
+    		if(rand_Slime == currentSlimeNumber){ rand_Slime = Math.floor((Math.random() * 26) + 1); }
+    		currentSlimeNumber = rand_Slime;
     		$("#enemy").css("background-image", "url(./assets/images/ghosts/"+rand_Slime+".png)");
 
 
@@ -592,7 +649,7 @@ $(document).ready (function(){
 	    			$( "#spellText" ).empty();
 	    			$( "#enemy_H" ).attr("class", "hideEnemy");
 
-		    		gameFunctions("resetKeys");
+		    		keyAI("resetKeys");
 
 		    		setTimeout(function(){ gameFunctions("newLevel"); }, 500);
 
